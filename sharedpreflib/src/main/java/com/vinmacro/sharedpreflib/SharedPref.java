af665117c152d1061e29b5e4339f8a0178bd29da
Vinoth Ragavan <vinothprofessional@outlook.com>
2019-03-08 07:39:27 +0530

    Initial commit

---
+++ sharedpreflib/src/main/java/com/vinmacro/sharedpreflib/SharedPref.java
@@ -0,0 +1,34 @@
+package com.vinmacro.sharedpreflib;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import javax.inject.Singleton;
+import dagger.Provides;
+import dagger.Module;
+
+
+
+@Module
+public class SharedPref {
+
+    private Context context;
+
+
+    public SharedPref(Context context){
+        this.context = context;
+    }
+    
+    @Singleton
+    @Provides
+    public Context provideContext() {
+        return context;
+    }
+
+    @Singleton
+    @Provides
+    public SharedPreferences provideSharedPreferences(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context);
+    }
+}
